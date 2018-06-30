d3a8636d9e805d96094bef37c8600172f59e3d3a
Justin Schwausch <justinschwausch980@gmail.com>
2018-06-29 22:01:58 -0500

    Working

--- UltraSonic.srcs/sources_1/new/top.v
+++ UltraSonic.srcs/sources_1/new/top.v
@@ -24,58 +24,72 @@ module top(
 
 input clk,
 input JA0,
-input btnC,
-input[15:0] sw,
 output [3:0] an,
 output [6:0] seg,
 output JA1,
-output[4:0] led
+output[15:0] led
     );
     
     reg counten;
+    reg outen;
     reg[21:0] count;
-    reg[21:0] countf;
-    reg[4:0] dave;
+    reg[15:0] countf;
+    reg[15:0] sw;
+    reg[9:0] btncnt;
+    reg[25:0] delcnt = 26'b11111111111111111111111111;
+    wire btnC;
 
-    assign JA1 = btnC;
-    assign led = dave;
+    assign led = countf;
+    assign JA1 = outen;
+    assign btnC = (delcnt == 26'b0) ?1'b1:1'b0;
     
     seven_seg Useven_seg(
     
     .clk (clk),
-    .sw  (sw),
+    .sw  (countf),
     .an  (an),
     .seg (seg)
     );
     
+    always @(posedge clk)
+    begin
+    
+    if(btncnt != 10'b0000000000)
+    begin
+    btncnt = btncnt + 1'b1;
+    end
+    
+    else if(btnC == 1'b1)
+    begin
+    btncnt = 1'b1;
+    outen = 1'b1;
+    end
+    
+    if(btncnt == 10'b1111101000)
+    begin
+    btncnt = 10'b0000000000;
+    outen = 1'b0;
+    end
+    
+    delcnt <= delcnt + 1'b1;
+    
+    end
+    
     
     always @(posedge clk)
-    
-    if (JA0 == 1'b1)
+    begin
+    if (JA0)
     begin
         count <= count + 1;
     end
     
-    else if (JA0 <= 1'b0)
+    else if(count != 22'b0)
     begin
-        countf <=count;
-        count <= 22'b0;
-        if (countf == 22'b1110011111101111000000) //too far
-            dave <= 5'b00000;
     
-        else if (countf >= 22'b1000110110011010000000) //400 cm
-            dave <= 5'b00001;
-        
-        else if (countf >= 22'b0110101000110011100000) //300 cm
-            dave <= 5'b00010;
-        
-        else if (countf >= 22'b0100011011001101000000) //200 cm
-            dave <= 5'b00011;
-                
-        else if (countf >= 22'b0010001101100110100000) //100 cm
-            dave <= 5'b00100;
-                    
-
+        countf <= count[21:6];
+        count <= 22'b0;
     end
+
+  end
         
 endmodule
